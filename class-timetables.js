86f770708abb8e12508d9cd105401b30819a0518
bentheone <rben5838@gmail.com>
2024-02-27 11:53:59 +0200

    Added teachers module and timetables module and connected it to the index.js which is the generator and added freetimes

---
+++ class-timetables.js
@@ -0,0 +1,28 @@
+const timetables = [
+    {
+        name:'L4SOD A',
+        courses: []
+    },
+    {
+        name:'L4SOD B',
+        courses: ['Canisius', 'Canisius','David','David']
+    },
+    // {
+    //     name:'L3PWO',
+    //     courses: []
+    // },
+    {
+        name:'L5SOD A',
+        courses: []
+    },
+    {
+        name:'L4CSA',
+        courses: []
+    },
+    {
+        name:'L4WOT',
+        courses: []
+    }
+]
+
+export { timetables }
